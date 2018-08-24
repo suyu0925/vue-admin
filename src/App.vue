690cad0b53aafb8c1d8050d0e1f8c9006b2f8fcc
lckof-mac <suyu0925@gmail.com>
2018-08-24 16:57:19 +0800

    添加404页面

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
   <div id="app">
-    {{ hello }}
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data() {
-    return {
-      hello: 'hello, vue'
-    }
-  }
+  name: 'App'
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App'
+import router from './router'
+
+Vue.use(Element)
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
