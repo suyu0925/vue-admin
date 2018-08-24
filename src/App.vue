de7121d6bef84536303d56b484de55389d023d14
lckof-mac <suyu0925@gmail.com>
2018-08-24 15:49:03 +0800

    the first commit, init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    {{ hello }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      hello: 'hello, vue'
+    }
+  }
+}
+</script>
