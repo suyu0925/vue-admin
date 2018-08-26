ad3012f888d262bd1d9f8e5feee90e333347c398
suyu0925 <suyu0925@gmail.com>
2018-08-27 00:33:30 +0800

    原因是没有import svg文件，搞定svg的问题

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import SvgIcon from '@/components/SvgIcon'// svg component
 
+// requires and returns all modules that match
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+
+// import all svg
+const req = require.context('./svg', true, /\.svg$/)
+requireAll(req)
+
 // register globally
 Vue.component('svg-icon', SvgIcon)
